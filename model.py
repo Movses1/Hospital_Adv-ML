36806cb7a19c5bfd7cdbf6ace65639a024677f56
Movses11 <movses.ghazaryan@gmail.com>
2023-04-18 21:50:54 +0400

    Model class with multiple stacked estimators. This can be drastically changed, it's not final

--- model.py
+++ model.py
@@ -4,12 +4,24 @@
 from preprocessor import Preprocessor as prpr
 import numpy as np
 import pandas as pd
-import sklearn
+
+from sklearn.ensemble import StackingClassifier, AdaBoostClassifier, RandomForestClassifier, BaggingClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 
 class Model:
     def __init__(self):
-        pass
-    def fit(self):
-        pass
-    def predict(self):
-        pass
+        estimators = [
+            ('RF', RandomForestClassifier(n_estimators=100, random_state=1)),
+            ('ADB', AdaBoostClassifier(n_estimators=100, random_state=1)),
+            ('bag_clf', BaggingClassifier(n_estimators=100, random_state=1)),
+            ('grad_b', GradientBoostingClassifier())
+        ]
+        self.model = StackingClassifier(estimators=estimators, final_estimator=RandomForestClassifier())
+
+    def fit(self, X, Y):
+        self.model.fit(X, Y) # sample_weights = ..., shape: n_samples
+        return self.model.score(X, Y)
+
+    def predict(self, X):
+        return self.model.predict(X)
+
