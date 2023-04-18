dcc059a76b75c37cb3a8c1a1be9347b84120359d
Movses11 <movses.ghazaryan@gmail.com>
2023-04-18 15:50:52 +0400

    some minor bug fixes

--- preprocessor.py
+++ preprocessor.py
@@ -10,10 +10,12 @@ from sklearn.preprocessing import MinMaxScaler
 class Preprocessor:
     def __init__(self):
         self.df_orig = None  # train dataframe
+        self.scaler = MinMaxScaler()  # Warning: scaler should be fitted only on train data
+        self.scaler.set_output(transform='pandas')
+        self.df_orig_tr = None  # original df but transformed
+
         self.orig_means = None  # mean values for each column in train data
         self.orig_medians = None  # median values for each column in train data
-        self.scaler = MinMaxScaler()  # Warning: scaler should be fitted only on train data
-        self.df_orig_tr = None  # original df but transformed
         self.gender_heights = np.array([160.5, 176])
 
         self.unique_tests = []  # unique medical test names that have extensions
@@ -27,17 +29,16 @@ class Preprocessor:
         :return: df_new but transformed
         """
         self.df_orig = df_new
-        if 'In-hospital_death' in df.columns:
+        if 'In-hospital_death' in self.df_orig.columns:
             self.df_orig.drop('In-hospital_death', axis=1, inplace=True)
         self.orig_means = self.df_orig.mean(axis=0)
         self.orig_medians = self.df_orig.median(axis=0)
-        self.df_orig_tr = df.copy()
 
         # creating _isna columns for tests with extensions
         test_name = np.array([i[:i.find('_')] for i in df_new.columns if '_' in i])
         test_name_singular = np.array([i for i in df_new.columns if '_' not in i])
-        self.unique_tests = np.unique(test_name)
-        self.unique_tests_singular = np.unique(test_name_singular)
+        self.unique_tests = np.unique(test_name).astype('object')
+        self.unique_tests_singular = np.unique(test_name_singular).astype('object')
         has_nans = df_new[self.unique_tests_singular].isna().any()
         self.unique_tests_singular = self.unique_tests_singular[has_nans]
 
@@ -52,17 +53,21 @@ class Preprocessor:
         transforms the original dataframe by adding new columns and handling nan values
         :return: df_transformed
         """
-        df_new.Gender.fillna(1, inplace=True)
+        gender_nans=df_new.Gender.isna()
+        df_new.loc[gender_nans, 'Gender'] = np.random.randint(2, size=gender_nans.sum())
         df_transformed = df_new.copy()
         height_nans = df_transformed.Height.isna()
-        df_transformed[height_nans].Height = self.gender_heights[df_transformed[height_nans].Gender.values.astype('int32')]
+        df_transformed.loc[height_nans, 'Height'] = self.gender_heights[df_transformed[height_nans].Gender.values.astype('int32')]
+        df_transformed['Height_isna'] = height_nans
 
-        df_transformed[self.unique_tests + '_fl_isna'] = df_new[self.unique_tests + '_first'].isna()
-        df_transformed[self.unique_tests_singular] = df_new[self.unique_tests_singular].isna()
+        df_temp = df_new[self.unique_tests + '_first'].isna()
+        df_temp.columns = self.unique_tests + '_fl_isna'
+        df_transformed = pd.concat([df_transformed, df_temp], axis=1)
+
+        df_temp = df_new[self.unique_tests_singular].isna()
+        df_temp.columns = self.unique_tests_singular + '_isna'
+        df_transformed = pd.concat([df_transformed, df_temp], axis=1)
 
         # you choose how you fill the nans
-        df_transformed.fillna(self.orig_means, inplace=True)
+        # df_transformed.fillna(self.orig_means, inplace=True)
         return df_transformed
-
-
-df = pd.read_csv('hospital_deaths_train.csv')
