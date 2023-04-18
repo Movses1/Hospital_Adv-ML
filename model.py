194c4b9687004ec8f1e8d6c62a5cf8a5fd68301e
Movses11 <movses.ghazaryan@gmail.com>
2023-04-18 15:53:22 +0400

    initial frame for model class

--- model.py
+++ model.py
@@ -1,2 +1,15 @@
 # this should include class named Model, which should include fit and predict functions.
 # It should be considered, that the input is already preprocessed here.
+
+from preprocessor import Preprocessor as prpr
+import numpy as np
+import pandas as pd
+import sklearn
+
+class Model:
+    def __init__(self):
+        pass
+    def fit(self):
+        pass
+    def predict(self):
+        pass
