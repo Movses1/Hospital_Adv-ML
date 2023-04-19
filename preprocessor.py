c23d27b8d3f45d58bb066e13df6126461974d314
Movses11 <movses.ghazaryan@gmail.com>
2023-04-19 21:57:27 +0400

    splitting into preprocessor into more functions

--- preprocessor.py
+++ preprocessor.py
@@ -16,7 +16,7 @@ class Preprocessor:
 
         self.orig_means = None  # mean values for each column in train data
         self.orig_medians = None  # median values for each column in train data
-        self.orig_mins = None
+        self.orig_min = None
         self.gender_heights = np.array([160.5, 176])
 
         self.unique_tests = []  # unique medical test names that have extensions
@@ -37,13 +37,7 @@ class Preprocessor:
         self.orig_medians = self.df_orig.median(axis=0)
         self.orig_mins = self.df_orig.min(axis=0)
 
-        # creating _isna columns for tests with extensions
-        test_name = np.array([i[:i.find('_')] for i in df_new.columns if '_' in i])
-        test_name_singular = np.array([i for i in df_new.columns if '_' not in i])
-        self.unique_tests = np.unique(test_name).astype('object')
-        self.unique_tests_singular = np.unique(test_name_singular).astype('object')
-        has_nans = df_new[self.unique_tests_singular].isna().any()
-        self.unique_tests_singular = self.unique_tests_singular[has_nans]
+        self.__save_isna_column_names(self.df_orig)
 
         self.df_orig_tr = self.transform(self.df_orig, fitting=True)
         return self.df_orig_tr
@@ -59,14 +53,28 @@ class Preprocessor:
         df_new.loc[gender_nans, 'Gender'] = np.random.randint(2, size=gender_nans.sum())
         df_transformed = df_new.copy()
 
-        df_transformed.loc[df_new.Height > 210, 'Height'] = np.nan
-        height_nans = df_transformed.Height.isna()
-        df_transformed.loc[height_nans, 'Height'] = self.gender_heights[
-            df_transformed[height_nans].Gender.values.astype('int32')]
-        df_transformed['Height_isna'] = height_nans
-        non_height_cols = self.unique_tests_singular != 'Height'
-        self.unique_tests_singular = self.unique_tests_singular[non_height_cols]
+        df_transformed = self.__handle_heights(df_transformed)
+        df_transformed = self.__add_isna_columns(df_transformed)
 
+        # you choose how you fill the nans
+        df_transformed.fillna(self.orig_means, inplace=True)
+        # df_transformed.fillna(self.orig_mins - 1, inplace=True)
+
+        if fitting:
+            self.scaler.fit(df_transformed)
+        return self.scaler.transform(df_transformed)
+
+    def __save_isna_column_names(self, df_new):
+        # creating _isna columns for tests with extensions
+        test_name = np.array([i[:i.find('_')] for i in df_new.columns if '_' in i])
+        test_name_singular = np.array([i for i in df_new.columns if '_' not in i])
+        self.unique_tests = np.unique(test_name).astype('object')
+        self.unique_tests_singular = np.unique(test_name_singular).astype('object')
+        has_nans = df_new[self.unique_tests_singular].isna().any()
+        self.unique_tests_singular = self.unique_tests_singular[has_nans]
+
+    def __add_isna_columns(self, df_new):
+        df_transformed = df_new.copy()
         df_temp = df_new[self.unique_tests + '_first'].isna()
         df_temp.columns = self.unique_tests + '_fl_isna'
         df_transformed = pd.concat([df_transformed, df_temp], axis=1)
@@ -75,9 +83,22 @@ class Preprocessor:
         df_temp.columns = self.unique_tests_singular + '_isna'
         df_transformed = pd.concat([df_transformed, df_temp], axis=1)
 
-        # you choose how you fill the nans
-        df_transformed.fillna(self.orig_mins - 1, inplace=True)
+        return df_transformed
 
-        if fitting:
-            self.scaler.fit(df_transformed)
-        return self.scaler.transform(df_transformed)
+    def __handle_heights(self, df_new):
+        """
+        :param df_new:
+        removes outliers, fills nans
+        :return:
+        """
+        df_transformed = df_new.copy()
+        df_transformed.loc[df_new.Height > 210, 'Height'] = np.nan
+        height_nans = df_transformed.Height.isna()
+        df_transformed.loc[height_nans, 'Height'] = self.gender_heights[
+            df_transformed[height_nans].Gender.values.astype('int32')]
+        df_transformed['Height_isna'] = height_nans
+
+        non_height_cols = self.unique_tests_singular != 'Height'
+        self.unique_tests_singular = self.unique_tests_singular[non_height_cols]
+
+        return df_transformed
